d8231459d176ead80b304c9de9ff3e9c5400d460
Christopher Tarkaa <88291960+christarkaa@users.noreply.github.com>
2024-06-11 16:37:24 +0100

    Update ATACSeq.sh

--- ATACSeq.sh
+++ ATACSeq.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env zsh
+
+# Make a new directory for the project and change to the directory
+mkdir ATAC-Seq && cd "$_"
+
+# Download datasets
+fastq-dump --split-files SRR24135553 SRR24135555
+
+# Make directories for quality control, Mapping and Peak Calling
+mkdir QC_Reports Mapping Peaks
+
+# Quality control using fastqc
 
