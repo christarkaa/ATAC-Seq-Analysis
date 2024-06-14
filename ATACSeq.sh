f0f0057d7f382d7f30e525bf31ac2ff2484163ae
Christopher Tarkaa <88291960+christarkaa@users.noreply.github.com>
2024-06-14 11:21:29 +0100

    Update ATACSeq.sh

--- ATACSeq.sh
+++ ATACSeq.sh
@@ -10,10 +10,10 @@ fastq-dump --split-files SRR24135553 SRR24135554 SRR24135555 SRR24135556
 mkdir QC_Reports Mapping Peaks
 
 # Quality control using fastqc
-SRR24135553_1.fastq SRR24135553_2.fastq -o QC_Reports
-SRR24135554_1.fastq SRR24135554_2.fastq -o QC_Reports
-SRR24135555_1.fastq SRR24135555_2.fastq -o QC_Reports
-SRR24135556_1.fastq SRR24135556_2.fastq -o QC_Reports
+fastqc SRR24135553_1.fastq SRR24135553_2.fastq -o QC_Reports
+fastqc SRR24135554_1.fastq SRR24135554_2.fastq -o QC_Reports
+fastqc SRR24135555_1.fastq SRR24135555_2.fastq -o QC_Reports
+fastqc SRR24135556_1.fastq SRR24135556_2.fastq -o QC_Reports
 
 # Summarizing the QC results
 multiqc QC_Reports
