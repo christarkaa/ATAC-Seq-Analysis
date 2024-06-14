37e61bf0b40d8ee5c683f0f254d1045edafcb6c7
Christopher Tarkaa <88291960+christarkaa@users.noreply.github.com>
2024-06-14 11:03:54 +0100

    Update ATACSeq.sh

--- ATACSeq.sh
+++ ATACSeq.sh
@@ -4,10 +4,17 @@
 mkdir ATAC-Seq && cd "$_"
 
 # Download datasets
-fastq-dump --split-files SRR24135553 SRR24135555
+fastq-dump --split-files SRR24135553 SRR24135554 SRR24135555 SRR24135556
 
 # Make directories for quality control, Mapping and Peak Calling
 mkdir QC_Reports Mapping Peaks
 
 # Quality control using fastqc
+SRR24135553_1.fastq SRR24135553_2.fastq -o QC_Reports
+SRR24135554_1.fastq SRR24135554_2.fastq -o QC_Reports
+SRR24135555_1.fastq SRR24135555_2.fastq -o QC_Reports
+SRR24135556_1.fastq SRR24135556_2.fastq -o QC_Reports
+
+# Summarizing the QC results
+multiqc QC_Reports
 
