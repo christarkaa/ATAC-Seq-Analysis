8fe946d614b6f646a89ecef64c7b039f256934f6
Christopher Tarkaa <88291960+christarkaa@users.noreply.github.com>
2024-06-15 19:59:09 +0100

    Update ATACSeq.sh

--- ATACSeq.sh
+++ ATACSeq.sh
@@ -35,7 +35,7 @@ cutadapt -a CTGTCTCTTATACACATCT -A CTGTCTCTTATACACATCT \
 -o trimmed_SRR24135556_1.fastq -p trimmed_SRR24135556_2.fastq \
 --report=full SRR24135556_1.fastq SRR24135556_2.fastq > SRR24135556_report.txt
 
-Trimming with Sickle
+# Trimming with Sickle
 sickle pe -f trimmed_SRR24135553_1.fastq -r trimmed_SRR24135553_2.fastq \
 -t sanger -o quality_trimmed_SRR24135553_1.fastq -p quality_trimmed_SRR24135553_2.fastq \
 -s singletons_SRR24135553.fastq -q 20 -l 90
@@ -51,5 +51,15 @@ sickle pe -f trimmed_SRR24135555_1.fastq -r trimmed_SRR24135555_2.fastq \
 sickle pe -f trimmed_SRR24135556_1.fastq -r trimmed_SRR24135556_2.fastq \
 -t sanger -o quality_trimmed_SRR24135556_1.fastq -p quality_trimmed_SRR24135556_2.fastq \
 -s singletons_SRR24135556.fastq -q 20 -l 90
+
+# Mapping
+## Build reference genome index
+bowtie2-build GRCh38.p14.genome.fa hg38
+
+## Map reference genome index with the trimmed sequences
+bowtie2 -p 10 -x hg38 -1 quality_trimmed_SRR24135553_1.fastq -2 quality_trimmed_SRR24135553_2.fastq -S Mapping/SRR24135553.sam
+bowtie2 -p 10 -x hg38 -1 quality_trimmed_SRR24135554_1.fastq -2 quality_trimmed_SRR24135554_2.fastq -S Mapping/SRR24135554.sam
+bowtie2 -p 10 -x hg38 -1 quality_trimmed_SRR24135555_1.fastq -2 quality_trimmed_SRR24135555_2.fastq -S Mapping/SRR24135555.sam
+bowtie2 -p 10 -x hg38 -1 quality_trimmed_SRR24135556_1.fastq -2 quality_trimmed_SRR24135556_2.fastq -S Mapping/SRR24135556.sam
 
 
